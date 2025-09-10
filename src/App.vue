b3616cd1000201ffe5c0918bfe624720dd131ad9
Taxato <taxatoftw@gmail.com>
2025-09-10 17:17:51 +0200

    Setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="bg-dark h-screen py-40">
+		<h1 class="text-light font-playfair text-8xl text-center">
+			OSLO STRADA
+		</h1>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
