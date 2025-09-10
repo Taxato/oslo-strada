e90fc52389ff6afe892a04bb4057beb052350cce
Taxato <taxatoftw@gmail.com>
2025-09-11 01:17:02 +0200

    landing page wip

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
-	<div class="bg-dark h-screen py-40">
-		<h1 class="text-light font-playfair text-8xl text-center">
-			OSLO STRADA
+	<div class="h-screen background">
+		<h1
+			class="uppercase text-light font-playfair text-6xl text-center pt-[40rem] drop-shadow-sm drop-shadow-black/60">
+			Oslo Strada
 		</h1>
+		<h2
+			class="text-light pt-2 uppercase text-center text-3xl drop-shadow-sm drop-shadow-black/50">
+			Cycling club
+		</h2>
+		<h1
+			class="uppercase text-dark font-playfair text-6xl text-center pt-[40rem] drop-shadow-sm drop-shadow-light/60">
+			Oslo Strada
+		</h1>
+		<h2
+			class="text-dark pt-2 uppercase text-center text-3xl drop-shadow-sm drop-shadow-light/50">
+			Cycling club
+		</h2>
 	</div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style>
+.background {
+	background-image: url("background-mobile.jpeg");
+	background-position: cover;
+	background-attachment: fixed;
+}
+</style>
