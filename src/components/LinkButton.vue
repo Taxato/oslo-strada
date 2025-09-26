33e9dcf898a47e494ea54e3bca5ea14b02dee7e1
Taxato <taxatoftw@gmail.com>
2025-09-26 18:55:54 +0200

    Homepage wip

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,13 @@
+<template>
+	<a
+		:href="href"
+		class="text-light rounded-sm border px-4 py-2 text-center uppercase drop-shadow-sm drop-shadow-black/50">
+		<slot />
+	</a>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	href: string;
+}>();
+</script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,28 @@
 <template>
-	<div class="h-screen background bg-fixed bg-cover bg-no-repeat bg-center">
-		<header-light />
+	<div
+		class="background h-screen overflow-y-auto bg-cover bg-fixed bg-center bg-no-repeat pt-96 md:pt-32">
+		<bicycle-logo
+			class="fill-light stroke-light mx-auto mb-2 max-w-xl px-4" />
+		<header-light class="mb-4" />
+		<div class="mx-auto grid w-fit grid-cols-2 gap-4">
+			<link-button
+				href="https://www.strava.com/clubs/1458153"
+				class="justify-self-end">
+				Strava
+			</link-button>
+			<link-button
+				href="https://www.instagram.com/oslostrada/"
+				class="justify-self-start">
+				Instagram
+			</link-button>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import BicycleLogo from "../components/BicycleLogo.vue";
 import HeaderLight from "../components/HeaderLight.vue";
+import LinkButton from "../components/LinkButton.vue";
 </script>
 
 <style>
